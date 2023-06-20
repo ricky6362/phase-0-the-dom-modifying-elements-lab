7b62771203108058f236ffb8d80c543f4a377cf7
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-20 22:06:52 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+// Accessing the main through its id
+const main = document.getElementById('main')
+
+// removing the element
+main.remove()
+
+// Creating h1 element
+let newHeader = document.createElement("h1")
+
+// Appending the element
+document.body.appendChild(newHeader)
+
+// adding an id to the newHeader variable
+newHeader.setAttribute("id", "victory");
+
+// Adding text to the h1 element
+let userName = "Richard Kimani"
+newHeader.textContent = `${userName} is the champion`
